61a4186067e6c717737fb9e4e4dd3a309b06963c
lawlie8 <50700194+lawlie8@users.noreply.github.com>
2020-04-08 19:17:41 +0530

    /

--- tk.py
+++ tk.py
@@ -7,18 +7,16 @@ import sys
 import os
 import ctypes
 import random
+from tkinter import *
+from tkinter.ttk import *
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
 if is_admin():
-
-
     ctypes.windll.shcore.SetProcessDpiAwareness(1)
-
     #ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-
     window = tk.Tk() #creates window
     window.tk.call('tk', 'scaling', 2.0)
     window.geometry("1050x380")
@@ -26,22 +24,20 @@ if is_admin():
     button = tk.Button(text ="Change",height = 1,width=10,fg="#d6d6c2",bg="blue")
     entry  = tk.Entry(width=20)
     label = tk.Label(text = "MAcSpoof Mac Address Changer" ,fg="#d6d6c2",bg="#333338")
-
     label.place(relx=.5,rely=.5,anchor="c")
-
-
-
-
-    def entry(Addlist,Adapterlist):
+    def entry(Addlist,Adapterlist,adapter_label):
         #Choose mac address function here
         entry = tk.Entry()
-        entry.insert(0,"Insert your option here")
+        entry.insert(0," Insert your option here")
         def clear_entry():
             entry.place_forget()
             entry.destroy()
             entry1 = tk.Entry()
             entry1.pack(anchor='w')
+            entry1.focus()
             def change_mac_address(event):
+
+                #adapter_label.place_forget()
                 choice = entry1.get()
                 try:
                     choice = int(choice)
@@ -53,11 +49,18 @@ if is_admin():
                     if choice >= 1:
                         choice = int(choice)
                         print(choice)#mac change code must go here beacuse i fucked up the design
+                        progress = Progressbar(window,orient=HORIZONTAL,length=1010,mode='determinate')
+                        progress.place(anchor='w',x=20,y=350)
+                        #asd = input()
+                        progress['value'] = 10
+                        window.update_idletasks()
                         random_mac = "02"
                         list = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
                         for i in range(0,10):
                             x = random.choice(list)
                             random_mac = random_mac + x
+                        progress['value'] = 15
+                        window.update_idletasks()
                         list = [
                         r"powershell Get-ItemPropertyValue -Path 'HKLM:\\SYSTEM\\ControlSet001\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0000' -Name NetworkAddress",
                         r"powershell Get-ItemPropertyValue -Path 'HKLM:\\SYSTEM\\ControlSet001\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0001' -Name NetworkAddress",
@@ -86,16 +89,17 @@ if is_admin():
                         i2 = Addlist[choice - 1].split('-')
                         cscs = Addlist[choice -1]
                         i2 = ''.join(i2)
+                        time.sleep(0.5)
+                        progress['value'] = 50
+                        window.update_idletasks()
                         for i in list:
                             driver_description = i.replace('NetworkAddress','DriverDesc')
                             j = i.replace('NetworkAddress','OriginalNetworkAddress')
-
                             if os.popen(driver_description).read().split('\n')[0] == Adapterlist[choice - 1]:
                                 cou = os.popen(i).read().split('\n')
                                 cou = str(''.join(cou))
                                 cou = cou.split('-')
                                 cou = str(''.join(cou))
-
                                 if cou.lower() == i2.lower():
                                     set_value = i.replace('Get-ItemPropertyValue','Set-ItemProperty')
                                     try:
@@ -104,13 +108,11 @@ if is_admin():
                                         print('cant change value')
                                         time.sleep(5)
                                         exit()
-
                                 try:
                                     registry_value = os.popen(i).read().split('\n')[0] #test
                                     #print('registry_value = {}'.format(registry_value[33:62]))#test
                                     if registry_value[33:62] == 'NetworkAddress does not exist':
                                         new_item_entry = i.replace('Get-ItemPropertyValue','New-ItemProperty')
-
                                         os.system(new_item_entry + " -Value "+ random_mac + " -PropertyType 'String' ")
                                         print('Address Changed Restarting Devices')
                                         break
@@ -118,7 +120,8 @@ if is_admin():
                                         os.system(set_value + " -Value " + random_mac)
                                 except:
                                     print('no works on 139')
-
+                        progress['value'] = 70
+                        window.update_idletasks()
                         try:
                             os.system('powershell netsh interface set interface name="Wi-Fi" admin=disabled')
                             os.system('powershell netsh interface set interface name="Wi-Fi" admin=enabled')
@@ -131,12 +134,23 @@ if is_admin():
                             print("Press any key to Continue\n")
                             lahaghakjgh = input()
                             exit()
-                        #except:
-                        #    MessageBox = ctypes.windll.user32.MessageBoxW
-                        #    MessageBox(None, 'Wrong Choice', 'Window title', 0)
-                        #    print('128')
-                        #    entry1.delete(0,tk.END)
-
+                        progress['value'] = 100
+                        window.update_idletasks()
+                        MessageBox = ctypes.windll.user32.MessageBoxW
+                        MessageBox(None, 'Mac Address Changed to '+ random_mac, 'Window title', 0)
+                        entry1.delete(0,tk.END)
+                        adapter_label.place_forget()
+                        adapter_label.destroy()
+                        entry1.place_forget()
+                        entry1.destroy()
+                        button_choose.place_forget()
+                        button_choose.destroy()
+                        progress.place_forget()
+                        progress.destroy()
+                        adapter_list()
+                        '''***** ENDS HERE *****'''
+                        #window.destroy()
+                        '''***** ENDS HERE *****'''
                     else:
                         MessageBox = ctypes.windll.user32.MessageBoxW
                         MessageBox(None, 'Wrong Choice', 'Window title', 0)
@@ -146,16 +160,12 @@ if is_admin():
                     entry1.delete(0,tk.END)
                     MessageBox = ctypes.windll.user32.MessageBoxW
                     MessageBox(None, 'Wrong Choice', 'Window title', 0)
-                    #insert the thing that to be inserted here
                     print('140')
             entry1.bind('<Return>',change_mac_address)
             button_choose = tk.Button(text='Go',height=0,width=12,fg="white",bg="blue",padx=5,command=change_mac_address,master=window)
             button_choose.pack(pady=15,anchor='w')
-
         entry.pack(anchor='w')
         entry.after(2000,clear_entry)
-
-
     def adapter_list():
             dl = []
             ml = []
@@ -195,20 +205,14 @@ if is_admin():
                     Addlist.append(m)
             label_text = ""
             for i,k,h in zip(range(1,len(Addlist)+1),Addlist,Adapterlist):
-                #adapter_label = tk.Label(text=i+k+h)
-                #adapter_label.pack()
-
                 i,k,h = str(i),str(k),str(h)
                 label_text += i+"  :  "+k+"\t\t"+h+"\n"
-
             label_text = label_text.strip(" ")
             adapter_label = tk.Label(text = label_text,justify="left",fg='#d6d6c2',bg='#333338')
             adapter_label.pack(anchor='w',side ="left",padx=30)
             x.close()
             os.system('del ip.txt')
-            entry(Addlist,Adapterlist)
-
-
+            entry(Addlist,Adapterlist,adapter_label)
     def label3():
         label3 = tk.Label(text="Devices Found :",fg="#f7a483",bg='#333338')
         label3.place(x=10,y=100)
@@ -219,7 +223,6 @@ if is_admin():
         label2.pack()
         label2.after(1000,label3)
     label.after(3000,clear_label)
-
 else:
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
 window.mainloop()  #main window opens here
